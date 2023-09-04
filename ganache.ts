91393357f7abd5cb26d7ad8b470c7e47588ea088
Cadel Watson <cadel@geora.io>
2023-09-04 15:11:44 +1000

    chore: forge init

---
+++ ganache.ts
@@ -0,0 +1,38 @@
+import ganache = require("ganache");
+import * as ethers from "ethers";
+
+async function main() {
+  const options: ganache.ServerOptions<"ethereum"> = {
+    gasPrice: 0,
+    gasLimit: Number.MAX_SAFE_INTEGER,
+    allowUnlimitedContractSize: true,
+    vmErrorsOnRPCResponse: false, // Consistent with Besu
+    hardfork: "berlin",
+    chain: {
+      asyncRequestProcessing: false,
+    },
+    logging: {
+      verbose: true,
+    },
+    miner: {
+      instamine: "strict",
+    },
+  };
+
+  // @ts-ignore
+  const server = ganache.server(options);
+
+  server.listen(8777, function () {
+    console.log(`Ethereum RPC listening on port 8777`);
+
+    const provider = new ethers.providers.Web3Provider(server.provider);
+    provider.pollingInterval = 100;
+
+    provider.on("block", async (n) => {
+      const block = await provider.getBlock(n);
+      console.log(block);
+    });
+  });
+}
+
+main().catch((e) => console.log(e));
